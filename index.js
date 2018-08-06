d1f5f6b9dfe58e88d5ccc219a5aeb77f095331a0
aboudaman <adaman2000@gmail.com>
2018-08-06 12:03:07 -0400

    updated git ignore

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import parser from 'body-parser'
 
 const app = new express()
+
+app.use(parser.urlencoded({
+    extended: false
+}))
 
 app.get('/', (requestAnimationFrame, res) => {
     return res.send('hello Jello')
@@ -8,6 +13,10 @@ app.get('/', (requestAnimationFrame, res) => {
 
 app.get('/world', (req, res) => {
     return res.send('Getting World')
+})
+
+app.post('/', (req, res) => {
+    return res.send(req.body)
 })
 
 app.listen(process.env.PORT || 3000)
